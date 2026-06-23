8a563fcf39340838645280b3ba85866d757c69c5
Kavya2006 <68997893+Kavya2006@users.noreply.github.com>
2026-06-23 16:14:28 +0530

    Add Make It Increasing [B] from Codeforces

---
+++ 1675/B - Make It Increasing/solution.cpp
@@ -0,0 +1,165 @@
+// to find anything using keyword , use control+f
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int MOD = 1e9 + 7;
+ 
+// Types
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+ 
+// Shortcuts
+#define pb      push_back
+#define all(x)  begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x)   (int)((x).size())
+ 
+// Common containers
+using vi  = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vpi = vector<pii>;
+ 
+// ------------------------- Utility Functions ------------------------- //
+ 
+// ✅ GCD (Greatest Common Divisor)
+ll gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+ 
+// ✅ HCF (Same as GCD, for readability)
+ll hcf(ll a, ll b) {
+    if (b == 0)
+        return a;
+    else
+        return hcf(b, a % b);
+}
+ 
+// ✅ LCM (Least Common Multiple)
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    ll ans = (a / g) * b;
+    return ans;
+}
+ 
+// ✅ Fast Power (modular exponentiation)
+ll power(ll a, ll b, ll mod = MOD) {
+    ll res = 1;
+    while (b > 0) {
+        if (b % 2 == 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b = b / 2;
+    }
+    return res;
+}
+ 
+// ✅ Check if a number is prime
+bool isPrime(ll n) {
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    
+    for (ll i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+ 
+// ✅ nCr (Combination n choose r) using Fermat’s Little Theorem (for large n under mod)
+ll nCr_mod(ll n, ll r, ll mod = MOD) {
+    if (r > n)
+        return 0;
+ 
+    vector<ll> fact(n + 1, 1);
+    for (ll i = 2; i <= n; i++)
+        fact[i] = (fact[i - 1] * i) % mod;
+ 
+    ll numerator = fact[n];
+    ll denominator = (fact[r] * fact[n - r]) % mod;
+ 
+    ll result = (numerator * power(denominator, mod - 2, mod)) % mod;
+    return result;
+}
+ 
+// ✅ Simple nCr (without mod, for small numbers)
+ll nCr_simple(ll n, ll r) {
+    if (r > n)
+        return 0;
+    if (r == 0 || r == n)
+        return 1;
+ 
+    r = min(r, n - r);
+    ll res = 1;
+    for (ll i = 1; i <= r; i++) {
+        res = res * (n - r + i);
+        res = res / i;
+    }
+    return res;
+}
+ 
+ 
+// ---------------------------------------------------------
+// Check if string 's' is a substring of string 'x'
+bool isSub(const string &x, const string &s) {
+    return x.find(s) != string::npos;  
+    
+}
+ 
+bool compare(int a1 , int a2 ){
+ 
+ return a1>a2; // for decreasing
+ 
+}
+ 
+ 
+ 
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin>>t;
+    while(t--){
+    ll n; cin>>n;
+    vector<ll>a(n);
+   
+    for(int i=0 ; i<n ; i++){
+        cin>>a[i];
+    }
+    ll count =0 ; 
+    bool possible=true;
+ 
+    for(int i=n-1 ; i>0 ; i--){
+        if(!possible){
+            break;
+        }
+     while(a[i]<=a[i-1]){
+           a[i-1]=(a[i-1])/2;
+           count++;
+           if(a[i]==0){
+            possible = false;
+            break;
+           }
+     }
+ 
+    }
+    if(possible){
+        cout<<count<<endl;
+    }
+    else cout<<-1<<endl;
+  
+ 
+    }
+ 
+return 0;
+ 
+}
