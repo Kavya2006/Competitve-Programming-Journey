cf49f93daee7c20400eafdb1f9d8b9561f235949
Kavya Arora <68997893+Kavya2006@users.noreply.github.com>
2026-06-23 16:18:20 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main (){
+ll w; cin>>w;
+if(w==2){
+    cout<<"NO
+";
+}
+else if(w%2==0){
+    cout<<"YES
+";
+}
+else cout<<"NO
+";
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    return 0;
+}
