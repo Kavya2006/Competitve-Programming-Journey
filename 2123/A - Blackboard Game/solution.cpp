83a976c9571f67a1c73c630015494526066ad813
Kavya Arora <68997893+Kavya2006@users.noreply.github.com>
2026-06-23 16:24:24 +0530

    Add Blackboard Game [A] from Codeforces

---
+++ 2123/A - Blackboard Game/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long ;
+int main (){
+  ll t; cin>>t;
+  while(t--){
+    ll n; cin>>n;
+if(n%4==0){
+    cout<<"Bob"<<endl;
+}
+else{
+    cout<<"Alice"<<endl;
+}
+    
+    
+  }
+    return 0;
+}
