53f7cee151e1eb01814e84b3bc6d002a27461052
Kavya2006 <68997893+Kavya2006@users.noreply.github.com>
2026-06-23 16:07:33 +0530

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main (){
+int t; cin>>t;
+int n;
+for(int i=0 ; i<t ; i++) {
+ cin>>n ; 
+ if(n%3==0 ) {
+    cout<<"Second"<<endl;
+    
+ }
+ else cout<<"First"<<endl;
+}
+ 
+ 
+ 
+ 
+ 
+    return 0;
+}
