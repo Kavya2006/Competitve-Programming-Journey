f196a2bee2ac212fb657b5594e3f2b8c10e07009
Kavya Arora <68997893+Kavya2006@users.noreply.github.com>
2026-06-23 16:26:31 +0530

    Add Star [B] from Codeforces

---
+++ 171/B - Star/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+ 
+ 
+ 
+void solve() {
+ll a; cin>>a;
+cout<<6*a*(a-1)+1<<endl;
+}
+int main (){
+    ll t=1;
+ 
+    while(t--){
+        solve();
+    }
+ 
+}
